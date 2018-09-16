c9b8eb22fa0b7be70301e4e8c5e94b1190f6fb75
Yodgorbek Komilov <kyodgorbek@gmail.com>
2018-09-16 11:21:46 +0200

    added room dependcy and created database for saving goals

--- app/src/main/java/database/SavingsRoomDatabase.java
+++ app/src/main/java/database/SavingsRoomDatabase.java
@@ -1,30 +1,85 @@
 package database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.db.SupportSQLiteOpenHelper;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.DatabaseConfiguration;
 import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
 import pojo.SavingsGoal;
 
 @Database(entities = {SavingsGoal.class}, version = 1)
-public class SavingsRoomDatabase extends RoomDatabase {
-    @NonNull
-    @Override
-    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
-        return null;
+public abstract class SavingsRoomDatabase extends RoomDatabase {
+    public abstract SavingsGoalDao savingsGoalDao();
+
+    // marking the instance as volatile to ensure atomic access to the variable
+    private static volatile SavingsRoomDatabase INSTANCE;
+
+    static SavingsRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (SavingsRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            SavingsRoomDatabase.class, "savingsGoals_table")
+                            // Wipes and rebuilds instead of migrating if no Migration object.
+                            // Migration is not part of this codelab.
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
     }
 
-    @NonNull
-    @Override
-    protected InvalidationTracker createInvalidationTracker() {
-        return null;
+    /**
+     * Override the onOpen method to populate the database.
+     * For this sample, we clear the database every time it is created or opened.
+     *
+     * If you want to populate the database only when the database is created for the 1st time,
+     * override RoomDatabase.Callback()#onCreate
+     */
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+
+        @Override
+        public void onOpen (@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            // If you want to keep the data through app restarts,
+            // comment out the following line.
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    /**
+     * Populate the database in the background.
+     * If you want to start with more words, just add them.
+     */
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final SavingsGoalDao mDao;
+
+        PopulateDbAsync(SavingsRoomDatabase db) {
+            mDao = db.savingsGoalDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            mDao.deleteAll();
+
+            SavingsGoal savingsGoal = new SavingsGoal();
+            mDao.insert("goalImageUrl","targetAmount");
+
+
+            return null;
+        }
     }
 
-    @Override
-    public void clearAllTables() {
-
-    }
 }
+
